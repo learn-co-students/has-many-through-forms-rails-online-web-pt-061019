170666deb07b0d3683d4b42d35461edeca32babe
Brittany Javalera <brittjavalera@gmail.com>
2019-11-07 08:13:08 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
-
+  accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
@@ -9,5 +9,5 @@ class Post < ActiveRecord::Base
       self.post_categories.build(category: category)
     end
   end
-
+#This method is needed so duplicate categories are not created
 end
